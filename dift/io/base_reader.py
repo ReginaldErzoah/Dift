46cd1f5dced1bf533cf5d54844ac9808e441b9c6
Reginald Erzoah <reginalderzoah10@gmail.com>
2026-05-25 15:37:46 +0000

    batch update

--- dift/io/base_reader.py
+++ dift/io/base_reader.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from collections.abc import Iterator
 
 import polars as pl
 
@@ -17,3 +18,21 @@ class BaseReader(ABC):
     @abstractmethod
     def read(self, source: str) -> pl.DataFrame:
         """Read source into a Polars DataFrame."""
+
+    def supports_chunks(self, source: str) -> bool:
+        """
+        Return True if this reader supports chunked reading for the source.
+
+        Readers can override this when they support incremental loading.
+        """
+        return False
+
+    def read_chunks(self, source: str, chunk_size: int) -> Iterator[pl.DataFrame]:
+        """
+        Read source incrementally as Polars DataFrame chunks.
+
+        Readers that support chunked loading should override this method.
+        """
+        raise NotImplementedError(
+            f"{self.name} reader does not support chunked reading for source: {source}"
+        )
